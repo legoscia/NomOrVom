89958143d08496dd1452775a8dc2822dc2da681c
Richard D <richard@dutton.me.uk>
2015-09-04 19:03:38 +0100

    Parsing address out of restaurant linked page, food.gov.uk lookups still on -1 but very close.

--- chrome/addon/background.js
+++ chrome/addon/background.js
@@ -1,6 +1,6 @@
 var parseHTML = function(str) {
-	var tmp = document.implementation.createHTMLDocument();
-	tmp.body.innerHTML = str;
+	var tmp = document.implementation.createDocument();
+	tmp.documentElement.appendChild(str)
 	return tmp.body.children;
 }
 
@@ -39,16 +39,55 @@ chrome.runtime.onConnect.addListener(function(port){
 			console.log(restaurant.name + " " + restaurant.fullPageUri);
 			
 			var xhr = new XMLHttpRequest();
-			xhr.onreadystatechange = function() {
+			xhr.onload = function() {
 				if (xhr.readyState == 4)
 				{
-	//				console.log(xhr.responseText);
-					var pageDoc = parseHTML(xhr.responseText);
-					var addressELement = pageDoc.querySelector('span.address');
-					console.log(addressELement);
+					var pageDoc = xhr.responseXML;
+					var addressElement = pageDoc.querySelector('span.address');
+					//console.log(addressELement);
+
+					var streetAddress = "";
+					var streetAddressElement = addressElement.querySelector('span[itemprop="streetAddress"]');
+					if (streetAddressElement) streetAddress = streetAddressElement.innerHTML.trim();
+
+					var addressLocality = "";
+					var addressLocalityElement = addressElement.querySelector('span[itemprop="addressLocality"]')
+					if (addressLocalityElement) addressLocality = addressLocalityElement.innerHTML.trim(); 
+
+					var postcode = "";
+					var postcodeElement = addressElement.querySelector('span[itemprop="postalCode"]');
+					if (postcodeElement) postcode = postcodeElement.innerHTML.trim();
+
+					var restaurantAddress = streetAddress + " " + addressLocality + " " + postcode;
+					
+			  		var url = "http://api.ratings.food.gov.uk/Establishments?name=" + encodeURIComponent(restaurant.name) + "&address=" + encodeURIComponent(restaurantAddress); 
+
+					var rating = 0;
+
+					var xhrFood = new XMLHttpRequest();
+					xhrFood.onreadystatechange = function() {
+						if (xhrFood.readyState == 4)
+						{
+							var resp = JSON.parse(xhrFood.responseText);
+							if (resp.establishments.length > 0) {
+								rating = resp.establishments[0].RatingValue;
+							}
+							else {
+								rating = -1;
+							}
+							console.log(restaurant.id + " " + rating);
+							//port.postMessage({id:restaurant.id, rating:rating});
+						}
+					};
+					xhrFood.open("GET", url, true);
+					xhrFood.setRequestHeader('x-api-version', 2);
+					xhrFood.setRequestHeader('Content-Type','application/json');
+					xhrFood.setRequestHeader('Accept','application/json');
+					xhrFood.send();
 				}
 			};
-			xhr.open("GET", restaurant.fullPageUri, true);
+			xhr.open("GET", restaurant.fullPageUri);
+			xhr.responseType = "document";
 			xhr.send();
 	  	});
   	}
